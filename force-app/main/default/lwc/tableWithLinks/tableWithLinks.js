aecdedd0e5472d53431a73b4f80bedbc8d7256d8
kishoresoftware <kishorekumar5014@gmail.com>
2022-12-28 16:04:19 +0530

    commit pdf generation in lwc

--- force-app/main/default/lwc/tableWithLinks/tableWithLinks.js
+++ force-app/main/default/lwc/tableWithLinks/tableWithLinks.js
@@ -16,15 +16,11 @@ export default class TableWithLinks extends LightningElement {
         } else if (data) {
             this.dataList = data.map(element => {
                 var obj = Object.assign({}, element);
-                //this is for account name column
                 obj.idLink = '/' + obj.Id;
-                // this is for created by column
                 obj.createdByIdLink = '/' + obj.CreatedById;
                 obj.createdByName = obj.CreatedBy.Name;
-                //this is for parent account column
                 obj.parentIdLink = obj.ParentId ? '/' + obj.ParentId : null;
                 obj.parentName = obj.Parent?.Name;
-                //this is for owner column
                 obj.ownerIdLink = '/' + obj.OwnerId;
                 obj.ownerName = obj.Owner.Name;
                 return obj;
